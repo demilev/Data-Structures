0c89a3e8d791f9e2bf08a178923a3241f07b18ed
Dimitar Milev <milev.1996@gmail.com>
2016-11-09 21:56:50 +0200

    the implementation is getting heavier

--- BinaryTree/treesample.cpp
+++ BinaryTree/treesample.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <assert.h>
 #include <string.h>
-#include <algorithm> 
+#include <algorithm>
 #include <vector>
 #include <string>
 
@@ -60,11 +60,21 @@ private:
 	void listLeaves(Node<T>*,vector<T>&) const;
 	void findTrace(const T&,Node<T>*,string&) const;
 	void prettyPrint(Node<T>*,int)const;
-	void findElem(int,Node<T>*,T*&);
 	void schemePrint(Node<T>*)const;
+	static Node<T>* copyTree (const Node<T>*);
+	void insertBOT (Node<T>*&,const T&);
+	Node<T>* insertedBOT (Node<T>*, const T& );
+	void deleteElement (Node<T>*&, const T&);
+	T minelement (Node<T>*) const;
+	vector<T> level(Node<T>*,int)const;
+	static Node<T>* deleted(Node<T>*,const T&);
+	bool isBOT(Node<T>*)const;
 public:
 	BTree();
-	BTree<T>& add (const T& data, const char *trace);
+	BTree(Node<T>*);
+	BTree (const BTree<T> &);
+	BTree<T>& operator = (const BTree<T> &);
+	BTree<T>& add (const T& , const char *);
 	void simplePrint () const;
 	void dottyPrint (ostream&);
 	void prettyPrint()const;
@@ -75,14 +85,310 @@ public:
 	int height() const;
 	int countLeaves() const;
 	T& maxLeaf() const;
-	T& getElement(const char *trace) const;
+	T& getElement(const char *) const;
 	void serialize (ostream&)const;
 	void deserialize (istream&);
 	vector<T> listLeaves() const;
 	~BTree();
 	string findTrace(const T&) const;
-	T& operator[](int);
+	BTree<T>& insertBOT (const T&);
+	BTree<T> insertedBOT (const T&);
+	void deleteElement (const T&);
+	T minelement ()const;
+	vector<T> level(int)const;
+	BTree<T> deletedBot(const T&)const;
+	bool isEmpty()const;
+	bool isBOT()const;
+
 };
+template<class T>
+bool BTree<T>::isBOT(Node<T>* subTreeRoot)const{
+	if(subTreeRoot==NULL)
+		return true;
+	if(subTreeRoot->left==NULL&&subTreeRoot->right==NULL)
+		return true;
+	if(subTreeRoot->left==NULL)
+		return subTreeRoot->data < subTreeRoot->right->data&&
+			   isBOT(subTreeRoot->right);
+	if(subTreeRoot->right==NULL)
+		return subTreeRoot->data >= subTreeRoot->left->data&&
+	   		   isBOT(subTreeRoot->left);
+	
+	return subTreeRoot->data < subTreeRoot->right->data&&
+		   subTreeRoot->data >= subTreeRoot->left->data&&
+		   isBOT(subTreeRoot->left)&&
+		   isBOT(subTreeRoot->right);
+
+}
+template<class T>
+bool BTree<T>::isBOT()const{
+	return isBOT(root);
+}
+template<class T>
+bool BTree<T>::isEmpty()const{
+	return root==NULL;
+}
+template<class T>
+BTree<T>::BTree(Node<T>* subTreeRoot){
+    root=copyTree(subTreeRoot);
+}
+template<class T>
+Node<T>* BTree<T>::deleted(Node<T>* subTreeRoot,const T& x){
+	if(subTreeRoot==NULL)
+		return NULL;
+	if(subTreeRoot->data==x&&
+	   subTreeRoot->left==NULL&&
+	   subTreeRoot->right==NULL)
+	{
+		return NULL;
+	}
+	if (x < subTreeRoot->data)
+	{
+		return new Node<T> (subTreeRoot->data,
+							deleted(subTreeRoot->left,x),
+							copyTree(subTreeRoot->right));
+	}
+
+
+	if (x > subTreeRoot->data)
+	{
+
+		return new Node<T> (subTreeRoot->data,
+							copyTree(subTreeRoot->left),
+							deleted(subTreeRoot->right,x));
+	}
+	if (subTreeRoot->right == NULL)
+	{
+		return subTreeRoot->left;
+	}
+
+	if (subTreeRoot->left == NULL)
+	{
+		return subTreeRoot->right;
+	}
+	if(subTreeRoot->data==x&&
+	   subTreeRoot->left!=NULL&&
+	   subTreeRoot->right!=NULL
+		)
+	{
+		BTree<T> temp(subTreeRoot->right);
+		T minel = temp.minelement(subTreeRoot->right);
+		return new Node<T> (minel,
+						copyTree(subTreeRoot->left),
+						deleted(subTreeRoot->right,minel));
+	}
+}
+template<class T>
+BTree<T> BTree<T>::deletedBot(const T& x)const{
+	BTree<T> result(deleted(root,x));
+	//result.root=deleted(root,x);
+	return result;
+}
+template<class T>
+vector<T> BTree<T>::level (Node<T>* subTreeRoot,int k)const{
+	if(subTreeRoot==NULL){
+		vector<T> result;
+		return result;
+	}
+	if(k==1){
+		vector<T> result;
+		result.push_back(subTreeRoot->data);
+		return result;
+	}
+	vector<T> leftResult=level(subTreeRoot->left,k-1);
+	vector<T> rightResult=level(subTreeRoot->right,k-1);
+	for(int i=0;i<rightResult.size();i++)
+		leftResult.push_back(rightResult[i]);
+	return leftResult;
+
+}
+template<class T>
+vector<T> BTree<T>::level (int k)const{
+	return level(root,k);
+}
+template<class T>
+void BTree<T>::deleteElement (Node<T> *&subTreeRoot, const T&x)
+{
+	//триене от празно дърво
+	if (subTreeRoot==NULL)
+		return;
+
+	//триене от листо
+	if (subTreeRoot->data == x &&
+		subTreeRoot->left == NULL &&
+		subTreeRoot->right == NULL)
+
+	{
+		delete subTreeRoot;
+		subTreeRoot = NULL;
+		return;
+	}
+
+	//триене от лявото поддърво
+	if (x < subTreeRoot->data)
+	{
+		deleteElement (subTreeRoot->left,x);
+		return;
+	}
+
+	//триене от дясното поддърво
+	if (x > subTreeRoot->data)
+	{
+		deleteElement (subTreeRoot->right,x);
+		return;
+	}
+
+	//вече сме сигурни, че трием корена!
+	//освен това сме сигурни, че корена има ПОНЕ ЕДИН
+	//наследник
+
+	//триене на корен само с 1 наследник
+	if (subTreeRoot->right == NULL)
+	{
+		Node<T> *tmp = subTreeRoot;
+		subTreeRoot = subTreeRoot->left;
+		delete tmp;
+		return;
+	}
+
+	//триене на корен само с 1 наследник
+	//този случй може да не се разглежда
+	if (subTreeRoot->left == NULL)
+	{
+		Node<T> *tmp = subTreeRoot;
+		subTreeRoot = subTreeRoot->right;
+		delete tmp;
+		return;
+	}
+
+
+	//триене на корена
+	//вече сме сигурни, че корена има точно два наследника
+	T minel = minelement(subTreeRoot->right);
+	subTreeRoot->data = minel;
+	deleteElement (subTreeRoot->right, minel);
+
+
+}
+
+template<class T>
+void BTree<T>::deleteElement (const T&x)
+{
+	deleteElement (root,x);
+}
+
+template<class T>
+T BTree<T>::minelement (Node<T> *subTreeRoot) const
+{
+	assert (subTreeRoot != NULL);
+	Node<T> *current = subTreeRoot;
+
+	while (current->left != NULL)
+	{
+		current = current->left;
+	}
+
+	return current->data;
+
+}
+
+
+template<class T>
+T BTree<T>::minelement () const
+{
+	return minelement (root);
+
+}
+
+template<class T>
+BTree<T> BTree<T>::insertedBOT (const T& x)
+{
+	BTree<T> result;
+	result.root = insertedBOT (root,x);
+
+	return result;
+}
+
+
+template<class T>
+Node<T>* BTree<T>::insertedBOT (Node<T>*subTreeRoot, const T& x)
+{
+	if (subTreeRoot == NULL)
+	{
+		return new Node<T> (x,NULL,NULL);
+	}
+
+	if (x > subTreeRoot->data)
+	{
+		return new Node<T> (subTreeRoot->data,
+							copyTree(subTreeRoot->left),
+							insertedBOT(subTreeRoot->right,x));
+
+	}
+
+	return new Node<T> (subTreeRoot->data,
+						insertedBOT(subTreeRoot->left,x),
+						copyTree(subTreeRoot->right));
+
+}
+
+
+template<class T>
+void BTree<T>::insertBOT (Node<T>* &subTreeRoot,const T& x)
+{
+
+	if (subTreeRoot == NULL)
+	{
+		subTreeRoot = new Node<T> (x,NULL,NULL);
+		return;
+	}
+
+	if (x <= subTreeRoot->data)
+	{
+		insertBOT (subTreeRoot->left,x);
+	} else {
+		insertBOT (subTreeRoot->right,x);
+	}
+
+}
+
+
+template<class T>
+BTree<T>& BTree<T>::insertBOT (const T& x)
+{
+	insertBOT (root,x);
+	return *this;
+}
+
+template<class T>
+BTree<T>& BTree<T>::operator = (const BTree<T> &other)
+{
+	if (this == &other)
+		return *this;
+
+	deleteAll (root);
+	root = copyTree (other.root);
+
+	return *this;
+}
+
+
+template<class T>
+Node<T>* BTree<T>::copyTree (const Node<T> *subTreeRoot)
+{
+	if (subTreeRoot == NULL)
+		return NULL;
+
+	return new Node<T> (subTreeRoot->data,
+		                copyTree(subTreeRoot->left),
+		                copyTree(subTreeRoot->right));
+}
+
+template<class T>
+BTree<T>::BTree (const BTree<T> &other)
+{
+	root = copyTree (other.root);
+}
 template<class T>
 void BTree<T>::schemePrint (Node<T> *subTreeRoot) const
 {
@@ -104,26 +410,6 @@ void BTree<T>::schemePrint () const
 {
 	schemePrint (root);
 	cout << endl;
-}
-template<class T>
-void BTree<T>::findElem(int i,Node<T>*subTreeRoot,T*& elem){
-	if(i==0){
-		assert(subTreeRoot!=NULL);
-		elem=&subTreeRoot->data;
-		return;
-	}
-	if(subTreeRoot->left!=NULL){
-		findElem(i-1,subTreeRoot->left,elem);
-	}
-	if(subTreeRoot->right!=NULL)
-		findElem(i-1,subTreeRoot->right,elem);
-	return;
-}
-template<class T>
-T& BTree<T>::operator[](int i){
-	T* result;
-	findElem(i,root,result);
-	return *result;
 }
 template<class T>
 void BTree<T>::prettyPrint()const{
@@ -228,19 +514,19 @@ void BTree<T>::dottyPrint (Node<T> *subTreeRoot,ostream& out) const
 	if (subTreeRoot == NULL)
 		return;
 
-	out << subTreeRoot->getID() 
-	    << "[label=\"" 
-	    << subTreeRoot->data 
+	out << subTreeRoot->getID()
+	    << "[label=\""
+	    << subTreeRoot->data
 	    << "\"];" << endl;
 
 	if (subTreeRoot->left != NULL)
-		out << subTreeRoot->getID() 
+		out << subTreeRoot->getID()
 	        <<"->"
 	        << subTreeRoot->left->getID()
 	        << endl;
 
 	if (subTreeRoot->right != NULL)
-		out << subTreeRoot->getID() 
+		out << subTreeRoot->getID()
 	        <<"->"
 	        << subTreeRoot->right->getID()
 	        << endl;
@@ -265,7 +551,7 @@ T& BTree<T>::getElement(const char *trace, Node<T>* subTreeRoot) const
 	if (strlen(trace) == 0){
 		return subTreeRoot->data;
 	}
-	
+
 	assert(strlen(trace)>0);
 
 	if (trace[0] == 'L')
@@ -293,7 +579,7 @@ T& BTree<T>::maxLeaf(Node<T>* subTreeRoot) const{
 	else{
 		return maxLeaf(subTreeRoot->right);
 	}
-		
+
 }
 template<class T>
 T& BTree<T>::maxLeaf() const{
@@ -335,7 +621,7 @@ template<class T>
 int BTree<T>::searchCount(bool(*pred)(const T&),Node<T> *subTreeRoot){
 	if (subTreeRoot == NULL)
 		return 0;
-	
+
 	if (pred(subTreeRoot->data))
 		return 1 + searchCount(pred, subTreeRoot->right) + searchCount(pred, subTreeRoot->left);
 	else
@@ -460,7 +746,7 @@ void removeWhite (istream &in)
 template <class T>
 Node<T>* BTree<T>::parseTree (istream &in)
 {
-	
+
 	removeWhite (in);
 
 	if (in.peek() == 'n')
@@ -496,7 +782,7 @@ void testMember ()
 {
 	BTree<int> t;
 
-	t.add(10,"").add(12,"L").add(14,"R").add(15,"LR");	
+	t.add(10,"").add(12,"L").add(14,"R").add(15,"LR");
 
 	assert (t.member(12) == true);
 	assert (t.member(18) == false);
@@ -516,33 +802,20 @@ bool isEven(const int& x){
 }
 int main () //some testing
 {
-	
+
 	testMember ();
 
 	BTree<int> t;
 
-	t.add(10, "").add(12, "L").add(14, "R").add(15, "LR").add(23, "LRL").add(24, "RR").add(27, "RRR").add(28, "RRRR").add(50,"LRR").add(50,"LL").add(2000,"RRRL");
-	t.simplePrint();
-	
-	t.map (plusOne);
+	t.insertBOT(10).insertBOT(23).insertBOT(18).insertBOT(4).insertBOT(32).insertBOT(29).insertBOT(7).insertBOT(17).insertBOT(40).insertBOT(2).add(11,"LLL");
 
-	t.simplePrint ();
-	cout << endl;
-	cout << t.getElement("");
-	cout << endl;
-	cout << t.getElement("L");
-	cout << endl;
-	cout << t.getElement("RRRR");
-	cout<<endl;
-	vector<int> leaves=t.listLeaves();
-	for(int i=0;i<leaves.size();i++)
-		cout<<leaves[i]<<" ";
-	cout<<endl;
-	cout<<t.findTrace(28)<<endl;
-	t.prettyPrint();
+
+	//t.prettyPrint();
 	t.dottyPrint(cerr);
-	cout<<endl;
-	t.schemePrint();
+	BTree<int> t1=t.deletedBot(23);
+	t1.prettyPrint();
+	t.simplePrint();
+	cout<<t.isBOT()<<endl;
 	system("pause");
 	return 0;
 }
